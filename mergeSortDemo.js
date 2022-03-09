3f932d09fc9fd99eb966445ca83399e12937d5cd
=Eric Niemeyer <=niemeyer.eric@gmail.com>
2022-03-09 11:29:27 -0500

    merged

--- mergeSortDemo.js
+++ mergeSortDemo.js
@@ -15,7 +15,29 @@ const combine = (l, r) => {
     }
 };
 
-left = [1, 2, 5,10];
-right = [3, 4, 6, 20];
+const mergeSort = (a) => {
+    console.log(a);
+    // base case 0 or 1 items
+    if (a.length <= 1) return a;
 
-console.log(combine(left, right));
+    // split into l and r
+    let l = a.slice(0, Math.floor(a.length / 2));
+    let r = a.slice(Math.floor(a.length / 2));
+
+    l = mergeSort(l);
+    r = mergeSort(r);
+
+    a = combine(l,r);
+    console.log(`Combined L:${l} and R:${r}`);
+    // console.log(l);
+    // console.log(r);
+
+    return a;
+};
+
+left = [1, 2, 5, 10];
+right = [3, 4, 6, 20];
+long = [1, 3, 2, 4,100, 6, 8, 0,-1];
+
+// console.log(combine(left, right));
+console.log(mergeSort(long));
